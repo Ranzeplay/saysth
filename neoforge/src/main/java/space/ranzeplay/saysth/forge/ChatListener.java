4e02b6a5f514fc59c46793c1d857352ecb4d6f46
Jeb Feng <ranzeplay@outlook.com>
2024-11-08 08:44:25 +0800

    Fix issues in migrating to NeoForge

--- neoforge/src/main/java/space/ranzeplay/saysth/forge/ChatListener.java
+++ neoforge/src/main/java/space/ranzeplay/saysth/forge/ChatListener.java
@@ -1,18 +1,15 @@
 package space.ranzeplay.saysth.forge;
 
-import net.minecraftforge.event.ServerChatEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
-import space.ranzeplay.saysth.Main;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.neoforge.event.ServerChatEvent;
 import space.ranzeplay.saysth.events.PlayerChatEvent;
 
 import java.io.IOException;
 
-@Mod.EventBusSubscriber(modid = Main.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class ChatListener {
     @SubscribeEvent
-    public static void onPlayerChat(final ServerChatEvent event) throws IOException, InterruptedException {
-        MainForge.LOGGER.info("Player chat");
+    public void onPlayerChat(final ServerChatEvent event) throws IOException {
+        MainNeoForge.LOGGER.info("Player chat");
         PlayerChatEvent.onPlayerChat(event.getPlayer(), event.getMessage().getString());
     }
 }
---
+++ neoforge/src/main/java/space/ranzeplay/saysth/forge/MainNeoForge.java
@@ -0,0 +1,24 @@
+package space.ranzeplay.saysth.forge;
+
+import com.mojang.logging.LogUtils;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.loading.FMLPaths;
+import net.neoforged.neoforge.common.NeoForge;
+import org.slf4j.Logger;
+import space.ranzeplay.saysth.Main;
+
+import static space.ranzeplay.saysth.Main.MOD_ID;
+
+@Mod(MOD_ID)
+public final class MainNeoForge {
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    public MainNeoForge() {
+        NeoForge.EVENT_BUS.register(new ChatListener());
+
+        final var configDir = FMLPaths.CONFIGDIR.get();
+
+        // Run our common setup.
+        Main.init(configDir, LOGGER);
+    }
+}
