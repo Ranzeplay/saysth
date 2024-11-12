e866f985002a3b3700e8143ef941d541b367fe44
Jeb Feng <ranzeplay@outlook.com>
2024-11-12 16:26:25 +0800

    Fix docs versioning

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -40,13 +40,6 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl: "https://github.com/Ranzeplay/saysth/tree/main/docs",
-          lastVersion: "current",
-          versions: {
-            current: {
-              label: "Dev",
-              path: "dev",
-            },
-          },
         },
         blog: {
           showReadingTime: true,
--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -19,7 +19,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/dev/intro">
+            to="/docs/intro">
             View latest docs
           </Link>
           <Link
